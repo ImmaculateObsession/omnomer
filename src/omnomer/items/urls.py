41684b70aae9b6cff376399d4155cd0f9b02e8dd
Philip James <pjj@philipjohnjames.com>
2013-04-06 11:55:21 -0700

    Added the ability to delete, edit items.

--- src/omnomer/items/urls.py
+++ src/omnomer/items/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, include, url
 from items.views import (
     ListItemView,
     CreateItemView,
+    EditItemView,
+    DeleteItemView,
     )
 
 urlpatterns = patterns('',
     url(r'^list/', ListItemView.as_view(), name='items-list'),
     url(r'^create/', CreateItemView.as_view(), name='item-create'),
+    url(r'^edit/(?P<pk>\d+)/$', EditItemView.as_view(), name='item-edit'),
+    url(r'^delete/(?P<pk>\d+)/$', DeleteItemView.as_view(), name='item-delete')
 )
--- src/omnomer/items/views.py
+++ src/omnomer/items/views.py
@@ -30,3 +30,26 @@ class CreateItemView(CreateView):
         context['action'] = reverse('item-create')
 
         return context
+
+class EditItemView(UpdateView):
+
+    model = Item
+    template_name = 'edit_item.html'
+
+    def get_success_url(self):
+        return reverse('items-list')
+
+    def get_context_data(self, **kwargs):
+
+        context = super(EditItemView, self).get_context_data(**kwargs)
+        context['action'] = reverse('item-edit', kwargs={'pk': self.get_object().id})
+
+        return context
+
+class DeleteItemView(DeleteView):
+
+    model = Item
+    template_name = 'item_delete.html'
+
+    def get_success_url(self):
+        return reverse('items-list')
