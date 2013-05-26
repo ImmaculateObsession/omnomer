afabd15d376effa9a6b7ed827de42cdd1ed00d25
Philip James <pjj@philipjohnjames.com>
2013-05-26 10:18:58 -0700

    Added local secret key so we don't need to futz around in dev.

--- src/omnomer/omnomer/settings/local.py
+++ src/omnomer/omnomer/settings/local.py
@@ -5,15 +5,17 @@ DEBUG = True
 TEAMPLATE_DEBUG = DEBUG
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'omnomer',
-		'USER': '',
-		'PASSWORD': '',
-		'HOST': 'localhost',
-		'PORT': '',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'omnomer',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
+
+SECRET_KEY = '1234567890'
 
 INSTALLED_APPS += ('debug_toolbar', )
 INTERNAL_IPS = ('127.0.0.1',)
